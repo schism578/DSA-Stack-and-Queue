c2945e05cec0d87913f48fe6a4bff9b0d29a39e6
Adrian Marquis <adrian.marquis78@gmail.com>
2020-12-04 17:23:24 -0500

    submit

---
+++ queue.js
@@ -0,0 +1,148 @@
+class _Node {
+    constructor(data) {
+      this.data = data
+      this.next = null
+    }
+  }
+  
+  class Queue {
+    constructor() {
+      this.first = null
+      this.last = null
+      this.length = 0
+    }
+  
+    enqueue(item) {
+      const newNode = new _Node(item)
+      if(!this.first) {
+        this.first = newNode
+      }
+      else if(this.last) {
+        this.last.next = newNode
+      }
+      this.last = newNode
+      this.length++
+    }
+  
+    dequeue() {
+      if(this.first === null) {
+        return;
+      }
+      const node = this.first;
+      this.first = this.first.next;
+  
+      if(node === this.last) {
+        this.last = null;
+      }
+      this.length--
+      return node.data;
+    }
+  }
+  
+  function peek(queue) {
+    if(!queue.first) return null
+    console.log(queue.first)
+    return queue.first
+  }
+  
+  function isEmpty(queue) {
+    if(!queue.first && !queue.last) {
+      return true
+    }
+    return false
+  }
+  
+  function display(queue) {
+    let results = ''
+    let currNode = queue.first
+    if(!currNode) return 'Your queue is empty'
+    else if(!currNode.next) {
+      results += `${currNode.data}->null`
+      console.log(results)
+      return results
+    }
+    // if(!currNode.next) {
+    //   console.log(`${currNode.data}->null`)
+    //   return
+    // }
+    else {
+      while(currNode) {
+        results += `${currNode.data}->`
+        currNode = currNode.next
+      }
+      results += 'null'
+      console.log(results)
+      return results
+    }
+  }
+  
+  // const queue = new Queue()
+  // queue.enqueue(3)
+  // queue.enqueue(4)
+  // queue.dequeue()
+  // queue.dequeue()
+  // peek(queue)
+  // display(queue)
+  // console.log(isEmpty(queue))
+  // console.log(JSON.stringify(queue))
+  
+  const fQueue = new Queue()
+  const mQueue = new Queue()
+  
+  function pairDancers(dancers) {
+    dancers.forEach(dancer => {
+      if(dancer[0] === 'F') {
+        fQueue.enqueue(dancer)
+      }
+      else {
+        mQueue.enqueue(dancer)
+      }
+      if(fQueue.first && mQueue.first) {
+        let maleDancer = mQueue.dequeue()
+        let femDancer = fQueue.dequeue()
+        console.log(`${femDancer} is paired with ${maleDancer}`)
+      }
+    })
+    if(mQueue.length > 0) {
+      console.log(`There are ${mQueue.length} male dancers waiting to dance`)
+      } 
+      else if (fQueue.length > 0) {
+        console.log(`There are ${fQueue.length} female dancers waiting to dance`)
+      }
+  }
+  
+  // console.log(pairDancers([
+  //   'F Jane',
+  //   'M Frank',
+  //   'M John',
+  //   'M Sherlock',
+  //   'F Madonna',
+  //   'M David',
+  //   'F Beyonce'
+  // ]))
+  
+  //10. Ophidian Bank
+  
+  
+  const bankQueue = new Queue()
+  bankQueue.enqueue('Elan')
+  bankQueue.enqueue('Makua')
+  bankQueue.enqueue('Teddy')
+  bankQueue.enqueue('Fred')
+  
+  function ophidianBank(queue) {
+    while(queue.length > 0) {
+      if(Math.random() <= .25) {
+        let firstPerson = queue.first
+        queue.dequeue()
+        queue.enqueue(firstPerson.data)
+        console.log(`${firstPerson.data} has been moved to the end of the queue.`)
+      }
+      else {
+        console.log(`${queue.first.data} has been served.`)
+        queue.dequeue()
+      }
+    }
+  }
+  
+  ophidianBank(bankQueue)
---
+++ stack.js
@@ -0,0 +1,141 @@
+class _Node {
+    constructor(data, next=null) {
+      this.data = data
+      this.next = next
+    }
+  }
+  
+  class Stack {
+    constructor() {
+      this.top = null
+    }
+  
+    push(data) {
+      if(!this.top) {
+        this.top = new _Node(data)
+        return this.top
+      }
+      
+      const node = new _Node(data, this.top)
+      this.top = node
+    }
+  
+    pop() {
+      if(!this.top) {
+        return null;
+      }
+      let poppedItem = this.top;
+      this.top = this.top.next;
+      console.log(`popped: ${poppedItem.data}`)
+      return poppedItem.data;
+    }
+  }
+  
+  function peek(stack) {
+    if(!stack.top) {
+      return null
+    }
+    console.log(stack.top.data)
+    return stack.top
+  }
+  
+  function isEmpty(stack) {
+    console.log(stack.top === null)
+    return stack.top === null
+  }
+  
+  function display(stack) {
+    let results = '';
+    let currentNode = stack.top;
+    if (!currentNode) {
+      console.log('List is empty');
+      return;
+    }
+    if (currentNode.next === null) {
+      console.log(`${currentNode.data}->null`);
+      return;
+    }
+  
+    while (currentNode.next !== null) {
+      results += `${currentNode.data}->`;
+      currentNode = currentNode.next;
+    }
+    results += `${currentNode.data}->null`;
+    console.log(stack);
+    console.log(results);
+  }
+  
+  function is_palindrome(s) {
+    s = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+    // Your code goes here
+    let palindromeStack = new Stack()
+    let splitString = s.split('')
+    let stackLength = splitString.length
+    let forwardResults = ''
+    splitString.forEach(char => palindromeStack.push(char))
+    for (let i = stackLength; i > 0; i--) {
+      forwardResults += palindromeStack.pop()
+    }
+    console.log(s === forwardResults)
+    return s === forwardResults
+  }
+  
+  // input: '(3 + 3'=> 
+  function matchingParens(str) {
+    const stack = new Stack()
+    if(!str) return null;
+    for(let i=0; i<str.length; i++) {
+      if(str[i] === '(')
+        stack.push(str[i])
+      if(str[i]===')') {
+        if (stack.pop() === null)
+          return false
+      }
+    }
+    return(stack.top===null)
+  }
+  
+  const parensStack = new Stack()
+  console.log(matchingParens('((3 + 2))'))
+  
+  // console.log((matchingParens('3 + 3)')))
+  
+  // input: 3->1->2->4 => 1->2->3->4
+  function sortStack(stack) {
+    if(!stack.top.next) return stack;
+    display(stack)
+    let sortStack = new Stack()
+    while(stack.top) {
+      let temp = stack.pop()
+      while(sortStack.top && sortStack.top.data > temp) {
+        stack.push(sortStack.pop())
+      }
+      console.log(sortStack)
+      sortStack.push(temp)
+    }
+    display(sortStack)
+    return sortStack
+  }
+  
+  let numStack = new Stack()
+  // numStack.push(2)
+  // numStack.push(4)
+  // numStack.push(1)
+  // numStack.push(3)
+  // console.log(sortStack(numStack))
+  // True, true, true, false
+  // display((is_palindrome("dad")));
+  // console.log(is_palindrome("A man, a plan, a canal: Panamas"));
+  // display((is_palindrome("1001"))); 
+  // console.log(is_palindrome("Tauhida"));
+  // console.log(is_palindrome('Elan'))
+  
+  // let starTrek = new Stack();
+  // starTrek.push('Kirk')
+  // starTrek.push('Spock')
+  // starTrek.push('McCoy')
+  // // starTrek.push('Scotty')
+  // // starTrek.pop()
+  // // peek(starTrek)
+  // // isEmpty(starTrek)
+  // display(starTrek)
